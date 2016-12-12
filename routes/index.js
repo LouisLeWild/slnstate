41301409737b0d5266ad14044ea785352a11ac71
Paul Johnson <Paul Johnson>
2016-12-12 07:01:16 -0600

    added cache

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var NodeCache = require('node-cache');
+var solCache = new NodeCache();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,10 +11,28 @@ router.get('/', function(req, res, next) {
 
 router.post('/check', function(req, res, next){
 	var cookies = req.cookies;
-	console.log(cookies);
+	// console.log(cookies);
 	console.log(req.body);
+	console.log(solCache.get(req.body.index));
 	res.end();
 });
+
+function writePost(body){
+
+}
+
+function readData(){
+	return JSON.parse(fs.readFileSync('data.json', 'utf8'));
+}
+
+function initCache(){
+	var data = readData();
+	for(var p in data){
+		solCache.set(data[p].index, data[p].isChecked);
+	}
+}
+
+
 
 function circleFreeObject(obj, parentage, circleFree){
     for(var i in obj){
